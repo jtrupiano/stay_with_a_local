07ad04dd9d452048b04db0d0c5b83fe8176a3db7
John Trupiano <jtrupiano@gmail.com>
2010-04-19 17:55:41 -0400

    Split db setup from db seed

---
+++ db/setup.rb
@@ -0,0 +1,20 @@
+require "dm-core"
+require 'dm-serializer'
+
+# Load models
+$:.unshift File.join(File.dirname(__FILE__), "../lib/models")
+require "host"
+require "guest"
+require "room_request"
+
+configure :development, :test do
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup(:default, 'postgres://localhost/stay_with_a_local')
+  # DataMapper.setup(:default, 'mysql://localhost/stay_with_a_local')
+end
+
+configure :production do
+  load 'db/config.rb'
+end
+
+DataMapper.auto_migrate!
