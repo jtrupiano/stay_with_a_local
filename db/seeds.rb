57e8910e40d99445f82bf0d2cb8c1b78a897589e
John Trupiano <jtrupiano@gmail.com>
2010-04-25 12:12:22 -0400

    Add all hosts as guests in the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 hosts = YAML.load(File.read(File.join(File.dirname(__FILE__), "hosts.yml")))
 hosts.each do |h|
   host = Host.create h
+  Guest.create(:twitter => h['twitter'], :image_url => 'http://a3.twimg.com/profile_images/627637055/thumb_gravatar_normal.jpg', :name => h['name'])
 end
 
 g = Guest.create!(:name => 'John Trupiano', :twitter => 'jtrupiano', :image_url => 'http://a3.twimg.com/profile_images/627637055/thumb_gravatar_normal.jpg')
